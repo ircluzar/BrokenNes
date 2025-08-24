56f68ed14a8a1acf01dd58d7303e80d4f95806de
Ircluzar <phil@redscientist.com>
2025-08-24 14:35:21 -0400

    fixed corecount

--- Services/GameSaveService.cs
+++ Services/GameSaveService.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using Microsoft.JSInterop;
 using BrokenNes.Models;
 using NesEmulator;
+using NesEmulator.Shaders;
 
 namespace BrokenNes.Services;
 
@@ -9,10 +10,12 @@ public class GameSaveService
 {
     private const string StorageKey = "game_save_v1";
     private readonly IJSRuntime _js;
+    private readonly IShaderProvider _shaderProvider;
 
-    public GameSaveService(IJSRuntime js)
+    public GameSaveService(IJSRuntime js, IShaderProvider shaderProvider)
     {
         _js = js;
+        _shaderProvider = shaderProvider;
     }
 
     public async Task<GameSave> LoadAsync()
@@ -48,16 +51,34 @@ public class GameSaveService
         catch { }
     }
 
-    // Reflectively count available cores (CPU + PPU + APU) using CoreRegistry.
+    // Reflectively count available cores across all categories (CPU + PPU + APU + CLOCK + SHADER).
     public int GetUnlockedCoresCount()
     {
         try
         {
-            // Assume all discovered cores are unlocked for now.
-            var cpu = CoreRegistry.CpuIds?.Count ?? 0;
-            var ppu = CoreRegistry.PpuIds?.Count ?? 0;
-            var apu = CoreRegistry.ApuIds?.Count ?? 0;
-            return cpu + ppu + apu;
+        // Assume all discovered items are unlocked for now.
+        var cpu = CoreRegistry.CpuIds?.Count ?? 0;
+        var ppu = CoreRegistry.PpuIds?.Count ?? 0;
+        var apu = CoreRegistry.ApuIds?.Count ?? 0;
+        var clocks = 0; try { clocks = ClockRegistry.Ids?.Count ?? 0; } catch { clocks = 0; }
+        var shaders = 0; try { shaders = _shaderProvider?.All?.Count ?? 0; } catch { shaders = 0; }
+        return cpu + ppu + apu + clocks + shaders;
+        }
+        catch { return 0; }
+    }
+
+    // Total number of discoverable cores across all categories.
+    // For now this is identical to GetUnlockedCoresCount until we add locked core logic.
+    public int GetTotalCoresCount()
+    {
+        try
+        {
+        var cpu = CoreRegistry.CpuIds?.Count ?? 0;
+        var ppu = CoreRegistry.PpuIds?.Count ?? 0;
+        var apu = CoreRegistry.ApuIds?.Count ?? 0;
+        var clocks = 0; try { clocks = ClockRegistry.Ids?.Count ?? 0; } catch { clocks = 0; }
+        var shaders = 0; try { shaders = _shaderProvider?.All?.Count ?? 0; } catch { shaders = 0; }
+        return cpu + ppu + apu + clocks + shaders;
         }
         catch { return 0; }
     }
